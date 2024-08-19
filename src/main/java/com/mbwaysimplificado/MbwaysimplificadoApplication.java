13b046b8e5e281380cbbb440346764c7253155d8
Farllei Arcanjo <farllei.harrison@icloud.com>
2024-08-19 15:56:06 +0100

    create initial config

---
+++ src/main/java/com/mbwaysimplificado/MbwaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.mbwaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbwaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbwaysimplificadoApplication.class, args);
+	}
+
+}
